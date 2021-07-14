959f307f641d992cc5c3f01dbb448da825c3c340
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-07-15 01:13:14 +0700

    Update night_mode.py

--- night_mode.py
+++ night_mode.py
@@ -1,21 +1,15 @@
-# Copyright (C) 2020-2021 by DevsExpo@Github, < https://github.com/DevsExpo >.
-#
-# This file is part of < https://github.com/DevsExpo/FridayUserBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/DevsExpo/blob/master/LICENSE >
-#
-# All rights reserved.
+# 
 
-from main_startup.core.decorators import friday_on_cmd
-from main_startup import bot, Friday, Config
-from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
-from xtraplugins.dB.nightmodedb import is_night_chat_in_db, get_all_night_chats, rm_night_chat, add_night_chat
+from Petercord_Userbot.core.decorators import ilhammansiz_on_cmd
+from Petercord_Userbot import bot, Petercord_Userbot, Config
+from Petercord_Userbot.helper_func.basic_helpers import edit_or_reply, get_text
+from xtraPetercordUserbot.dB.nightmodedb import is_night_chat_in_db, get_all_night_chats, rm_night_chat, add_night_chat
 from pyrogram.types import ChatPermissions
-from main_startup.helper_func.logger_s import LogIt
+from Petercord_Userbot.helper_func.logger_s import LogIt
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 import logging
 
-@friday_on_cmd(
+@ilhammansiz_on_cmd(
     ["scgrp"],
     is_official=False,
     only_if_admin=True,
@@ -35,7 +29,7 @@ async def scgrp(client, message):
     await pablo.edit(f"**Added Chat {message.chat.title} With Id {message.chat.id} To Database. This Group Will Be Closed On 12Am(IST) And Will Opened On 06Am(IST)**")
 
 
-@friday_on_cmd(
+@ilhammansiz_on_cmd(
     ["rsgrp"],
     is_official=False,
     only_if_admin=True,
@@ -61,26 +55,26 @@ async def job_close():
         return
     for warner in lol:
         try:
-            await Friday.send_message(
-              int(warner.get("chat_id")), "`12:00 Am, Group Is Closing Till 6 Am. Night Mode Started !` \n**Powered By @FRidayOT**"
+            await Petercord_Userbot.send_message(
+              int(warner.get("chat_id")), "`12:00 Am, Group Is Closing Till 6 Am. Night Mode Started !` \n**Powered By @ilhammansiez**"
             )
-            await Friday.set_chat_permissions(warner.get("chat_id"), ChatPermissions())
+            await Petercord_Userbot.set_chat_permissions(warner.get("chat_id"), ChatPermissions())
             async for member in Friday.iter_chat_members(warner.get("chat_id")):
              if member.user.is_deleted:
                 try:
-                    await Friday.kick_chat_member(warner.get("chat_id"), member.user.id)
+                    await Petercord_Userbot.kick_chat_member(warner.get("chat_id"), member.user.id)
                 except:
                     pass
         except Exception as e:
             logging.info(str(e))
             ido = warner.get("chat_id")
             try:
-                await Friday.send_message(Config.LOG_GRP, f"[NIGHT MODE]\n\nFailed To Close The Group {ido}.\nError : {e}")
+                await Petercord_Userbot.send_message(Config.LOG_GRP, f"[NIGHT MODE]\n\nFailed To Close The Group {ido}.\nError : {e}")
             except:
                 pass
 
 
-scheduler = AsyncIOScheduler(timezone="Asia/Kolkata")
+scheduler = AsyncIOScheduler(timezone="Asia/Jakarta")
 scheduler.add_job(job_close, trigger="cron", hour=23, minute=55)
 scheduler.start()
 
@@ -90,10 +84,10 @@ async def job_open():
         return
     for warner in lol:
         try:
-            await Friday.send_message(
+            await Petercord_Userbot.send_message(
               int(warner.get("chat_id")), "`06:00 Am, Group Is Opening.`\n**Powered By @FRidayOT**"
             )
-            await Friday.set_chat_permissions(
+            await Petercord_Userbot.set_chat_permissions(
                         warner.get("chat_id"),
                         ChatPermissions(
                             can_send_messages=True,
@@ -112,6 +106,6 @@ async def job_open():
                 pass
             
 
-scheduler = AsyncIOScheduler(timezone="Asia/Kolkata")
+scheduler = AsyncIOScheduler(timezone="Asia/Jakarta")
 scheduler.add_job(job_open, trigger="cron", hour=6, minute=10)
 scheduler.start()
