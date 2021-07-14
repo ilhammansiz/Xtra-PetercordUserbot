c99b43065a9dae957baafe6fa03cea399b00f233
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-07-14 18:00:39 +0700

    Update subtitles.py

--- subtitles.py
+++ subtitles.py
@@ -1,10 +1,4 @@
-# Copyright (C) 2020-2021 by DevsExpo@Github, < https://github.com/DevsExpo >.
-#
-# This file is part of < https://github.com/DevsExpo/FridayUserBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/DevsExpo/blob/master/LICENSE >
-#
-# All rights reserved.
+# 
 
 import requests
 import os
@@ -12,10 +6,10 @@ import aiohttp
 import requests
 from bs4 import BeautifulSoup as bs
 import shutil
-from main_startup.config_var import Config
-from main_startup.core.decorators import friday_on_cmd
-from main_startup.core.startup_helpers import run_cmd
-from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
+from Petercord_Userbot.config_var import Config
+from Petercord_Userbot.core.decorators import ilhammansiz_on_cmd
+from Petercord_Userbot.core.startup_helpers import run_cmd
+from Petercord_Userbot.helper_func.basic_helpers import edit_or_reply, get_text
 BASE_URL = "https://isubtitles.org"
 
 async def search_sub(query):
@@ -67,7 +61,7 @@ async def get_lang(keyword):
                 link.append(f"{BASE_URL}{b.get('href')}")
     return index, language, link
 
-@friday_on_cmd(
+@ilhammansiz_on_cmd(
     ["subs", "substitle", "subdl"],
     cmd_help={
         "help": "Get Subtitle Of A Movie.",
