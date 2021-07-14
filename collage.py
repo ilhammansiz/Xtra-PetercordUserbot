239e3aee6b3738e43402f8996ecfaca49b96244d
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-07-15 01:50:31 +0700

    Update collage.py

--- collage.py
+++ collage.py
@@ -1,10 +1,4 @@
-# Copyright (C) 2020-2021 by DevsExpo@Github, < https://github.com/DevsExpo >.
-#
-# This file is part of < https://github.com/DevsExpo/FridayUserBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/DevsExpo/blob/master/LICENSE >
-#
-# All rights reserved.
+# 
 
 import glob
 import os
@@ -13,10 +7,10 @@ import shutil
 from PIL import Image
 import logging
 import pathlib
-from main_startup.config_var import Config
-from main_startup.core.decorators import friday_on_cmd
-from main_startup.core.startup_helpers import run_cmd
-from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
+from Petercord_Userbot.config_var import Config
+from Petercord_Userbot.core.decorators import ilhammansiz_on_cmd
+from Petercord_Userbot.core.startup_helpers import run_cmd
+from Petercord_Userbot.helper_func.basic_helpers import edit_or_reply, get_text
 
 
 async def create_s_collage(file_path, filename, width, stark_h):
@@ -73,7 +67,7 @@ async def create_s_collage(file_path, filename, width, stark_h):
 
 
 
-@friday_on_cmd(
+@ilhammansiz_on_cmd(
     ["collage"],
     cmd_help={
         "help": "Create Collage From All Images in A Chat.",
