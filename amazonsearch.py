653fc31ae7830c819eacbafdd2668c2fbf6ec309
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-07-15 01:24:32 +0700

    Update amazonsearch.py

--- amazonsearch.py
+++ amazonsearch.py
@@ -1,17 +1,10 @@
-# Copyright (C) 2020-2021 by DevsExpo@Github, < https://github.com/DevsExpo >.
-#
-# This file is part of < https://github.com/DevsExpo/FridayUserBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/DevsExpo/blob/master/LICENSE >
-#
-# All rights reserved.
-
+# 
 import requests
-from main_startup.core.decorators import friday_on_cmd
-from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
+from Petercord_Userbot.core.decorators import ilhammansiz_on_cmd
+from Petercord_Userbot.helper_func.basic_helpers import edit_or_reply, get_text
 
 
-@friday_on_cmd(
+@ilhammansiz_on_cmd(
     ["amsearch"],
     cmd_help={
         "help": "Search Products From Amazon!",
