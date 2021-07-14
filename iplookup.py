f70ec94880b4c18ad42228f49856744f1df2470f
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-07-15 01:54:19 +0700

    Update iplookup.py

--- iplookup.py
+++ iplookup.py
@@ -1,18 +1,12 @@
-# Copyright (C) 2020-2021 by DevsExpo@Github, < https://github.com/DevsExpo >.
-#
-# This file is part of < https://github.com/DevsExpo/FridayUserBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/DevsExpo/blob/master/LICENSE >
-#
-# All rights reserved.
+# 
 
-from main_startup.config_var import Config
-from main_startup.core.decorators import friday_on_cmd
-from main_startup.core.startup_helpers import run_cmd
-from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
+from Petercord_Userbot.config_var import Config
+from Petercord_Userbot.core.decorators import ilhammansiz_on_cmd
+from Petercord_Userbot.core.startup_helpers import run_cmd
+from Petercord_Userbot.helper_func.basic_helpers import edit_or_reply, get_text
 import aiohttp
 
-@friday_on_cmd(
+@ilhammansiz_on_cmd(
     ["ip"],
     cmd_help={
         "help": "Get Info About IP.",
