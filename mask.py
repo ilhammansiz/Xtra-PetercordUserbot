e29c54420e3a2df5faf04e76461b9ccd94ac0c00
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-07-15 01:28:35 +0700

    Update mask.py

--- mask.py
+++ mask.py
@@ -1,16 +1,11 @@
-# Copyright (C) 2020-2021 by DevsExpo@Github, < https://github.com/DevsExpo >.
-#
-# This file is part of < https://github.com/DevsExpo/FridayUserBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/DevsExpo/blob/master/LICENSE >
-#
-# All rights reserved.
+# 
 
-from main_startup.core.decorators import friday_on_cmd
-from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
+from Petercord_Userbot.core.decorators import ilhammansiz_on_cmd
+from Petercord_Userbot.helper_func.basic_helpers import edit_or_reply, get_text
 import asyncio
 import time
-@friday_on_cmd(
+
+@ilhammansiz_on_cmd(
     ["mask"],
     is_official=False,
     cmd_help={
